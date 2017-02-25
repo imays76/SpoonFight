485bd04559cc0e0838459ae66d79a5b5df90413f
hyunjik.bae <imays76@gmail.com>
2017-02-25 15:43:05 +0900

    1st

---
+++ WhoHasMore-unity/Assets/ButtonBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehavior : MonoBehaviour {
+    public GameObject ButtonText;
+    public int Score = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddScore()
+    {
+        Score = Score + 1;
+        ButtonText.GetComponentInChildren<Text>().text = "Score: " + Score.ToString();
+    }
+}
---
+++ WhoHasMore-unity/Assets/ChangeScene.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeScene: MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("scene1");
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene("scene0");
+    }
+}
