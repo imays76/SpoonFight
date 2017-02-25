51a6a21e565d2c907f729467e8fa95232341c332
hyunjik.bae <imays76@gmail.com>
2017-02-25 16:43:59 +0900

    added stop bgm feature.

---
+++ WhoHasMore-unity/Assets/StopBGM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopBGM : MonoBehaviour {
+    public AudioSource m_BGMSource;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Action()
+    {
+        m_BGMSource.Stop();
+    }
+}
